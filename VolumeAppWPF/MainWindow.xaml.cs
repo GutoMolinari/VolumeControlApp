26bc220afdeae34006844fa43309a674aba68791
GutoMolinari <gutomolinari@hotmail.com>
2021-01-09 03:23:24 -0300

    - Implemented scratch logic for the Riot Services controls; - Implemented globalHook on mouseEvents to toggle RiotServices mute state when pressing the mouseWheel; - Improved logic when closing the mainWindow;

--- VolumeAppWPF/MainWindow.xaml.cs
+++ VolumeAppWPF/MainWindow.xaml.cs
@@ -1,32 +1,59 @@
 ﻿using CoreAudio;
 using CoreAudio.Enums;
+using Gma.System.MouseKeyHook;
 using System;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace VolumeApp
 {
     public partial class MainWindow : Window
     {
-        //private const string valorantProcessName = "mpc-hc64";
-        //private const string valorantProcessName = "firefox";
         private const string valorantProcessName = "VALORANT-Win64-Shipping";
         private const string riotServicesProcessName = "RiotClientServices";
 
         private AudioSessionControl valorantAudioSession;
+        private AudioSessionControl riotServicesAudioSession;
+
         private readonly ActiveWindowHooker windowHooker = new ActiveWindowHooker();
+        private IKeyboardMouseEvents mkbEvents;
 
         public MainWindow()
         {
             InitializeComponent();
+
             btnAttachValorantAudio.Click += btnAttachValorantAudio_Click;
             btnToggleMuteValorantAudio.Click += btnToggleMuteValorantAudio_Click;
 
+            btnRiotClientServicesAudio.Click += btnRiotClientServicesAudio_Click;
+            btnToggleMuteRiotClientServicesAudio.Click += btnToggleMuteRiotClientServicesAudio_Click;
+
             windowHooker.ActiveWindowChanged += WindowHooker_ActiveWindowChanged;
-            Application.Current.Exit += delegate { windowHooker.Stop(); if (valorantAudioSession != null) valorantAudioSession.Mute = false; };
+            this.Closing += MainWindow_Closing;
         }
 
-        //
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            windowHooker.Stop();
+            UnsubscribeMouseEvents();
+
+            if (valorantAudioSession != null)
+            {
+                valorantAudioSession.Mute = false;
+                valorantAudioSession.Dispose();
+            }
+
+            if (riotServicesAudioSession != null)
+            {
+                riotServicesAudioSession.Mute = false;
+                riotServicesAudioSession.Dispose();
+            }
+        }
+
+        #region Valorant
+
+
         private void btnAttachValorantAudio_Click(object sender, RoutedEventArgs e)
         {
             var task = Task.Run(() =>
@@ -98,7 +125,6 @@ namespace VolumeApp
             if (valorantAudioSession == null)
                 return;
 
-
             if (e.WindowProcessId == valorantAudioSession.ProcessID)
             {
                 valorantAudioSession.Mute = false;
@@ -110,5 +136,111 @@ namespace VolumeApp
                 valorantAudioSession.Mute = true;
             }
         }
+
+
+        #endregion
+
+        #region RiotServices
+
+
+        private void btnRiotClientServicesAudio_Click(object sender, RoutedEventArgs e)
+        {
+            var task = Task.Run(() =>
+            {
+                return AudioSessionsHelper.GetAudioSessionByProcessName(riotServicesProcessName);
+            });
+
+            task.Wait();
+
+            if (task.Result == null)
+            {
+                txtRiotClientServicesAudioState.Text = "Not found";
+                return;
+            }
+
+            if (riotServicesAudioSession != null)
+            {
+                riotServicesAudioSession.Dispose();
+            }
+
+            riotServicesAudioSession = task.Result;
+
+            txtRiotClientServicesAudioState.Text = AudioSessionsHelper.GetStringAudioState(riotServicesAudioSession.State);
+            txtRiotClientServicesAudioMuted.Text = AudioSessionsHelper.GetStringFromMuted(riotServicesAudioSession.Mute);
+
+            riotServicesAudioSession.StateChanged += riotServicesAudioSession_StateChanged;
+            riotServicesAudioSession.SimpleVolumeChanged += riotServicesAudioSession_SimpleVolumeChanged;
+            SubscribeMouseEvents();
+        }
+
+        private void btnToggleMuteRiotClientServicesAudio_Click(object sender, RoutedEventArgs e)
+        {
+            if (riotServicesAudioSession == null)
+                return;
+
+            var newMute = !riotServicesAudioSession.Mute;
+
+            riotServicesAudioSession.Mute = newMute;
+            txtRiotClientServicesAudioMuted.Text = AudioSessionsHelper.GetStringFromMuted(newMute);
+        }
+
+        private void riotServicesAudioSession_StateChanged(object sender, AudioSessionStateChangedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                var state = e.NewState;
+
+                txtRiotClientServicesAudioState.Text = AudioSessionsHelper.GetStringAudioState(state);
+
+                if (state == AudioSessionState.AudioSessionStateExpired)
+                {
+                    riotServicesAudioSession.Mute = false;
+                    riotServicesAudioSession = null;
+                    UnsubscribeMouseEvents();
+                }
+            }));
+        }
+
+        private void riotServicesAudioSession_SimpleVolumeChanged(object sender, AudioSessionSimpleVolumeChangedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                txtRiotClientServicesAudioMuted.Text = AudioSessionsHelper.GetStringFromMuted(e.IsMuted);
+            }));
+        }
+
+        private void SubscribeMouseEvents()
+        {
+            mkbEvents = Hook.GlobalEvents();
+            mkbEvents.MouseClick += OnMouseClick;
+        }
+
+        private void UnsubscribeMouseEvents()
+        {
+            if (mkbEvents == null) return;
+
+            mkbEvents.MouseClick -= OnMouseClick;
+
+            mkbEvents.Dispose();
+            mkbEvents = null;
+        }
+
+        private void OnMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Middle)
+                return;
+
+            Task.Factory.StartNew(() =>
+            {
+                var newMute = !riotServicesAudioSession.Mute;
+                riotServicesAudioSession.Mute = newMute;
+
+                if (newMute)
+                    Console.Beep(1000, 100);
+            });
+        }
+
+
+        #endregion
     }
 }
